a9e5e83fc4e7a7cb544706e1a383dd79b2fe7e08
Михаил Родионов <47571243+midnightrebel@users.noreply.github.com>
2021-04-09 11:11:52 +0400

    Update solution.rb

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,43 +1,12 @@
 module Exercise
   module Arrays
     class << self
-      def replacemax(array)
-        array = Array.new(array)
-        i = 0
-        max = array[i]
-        while i < array.length do
-          if (max < array[i])
-            max = array[i]
-          end
-          if(array[i] >= 0)
-            array[i] = max
-            array[0] = max
-            array[1] = max
-          end
-          i += 1
-        end
-        return array
+      def replace(array)
+        array
       end
-     
+
       def search(_array, _query)
-        right = _array.length - 1
-        left = 0
-        middle = _array.length / 2
-        while(_array[middle] != _query && left < right)
-          if _array[middle] < _query
-            left = middle + 1
-          else
-            right = middle - 1
-          end
-          middle = (right + left) / 2
-        end
-        if _array[middle] == _query
-          return middle
-        end
-        if left > right
-          middle = -1
-          return middle
-        end
+        0
       end
     end
   end
