c0d6641e8200ab343a0d0ce6382cde16003b6d3c
midnight <grandcombo258@gmail.com>
2021-04-15 17:51:49 +0400

    fp solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,15 +1,19 @@
+
 module Exercise
   module Fp
     class << self
-      # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        sorted = _array.select { |row| row["rating_kinopoisk"].to_f > 0 && 
+          row["country"] != nil && 
+          row["country"].split(',').length > 1 }
+        result = sorted.reduce(0) { |sum, row| sum + row["rating_kinopoisk"].to_f} / sorted.count
+        return result
       end
-
+      
       def chars_count(_films, _threshold)
-        0
+        result = _films.select {|row|
+          row['rating_kinopoisk'].to_f > _threshold}.reduce(0) { |sum, row| sum + row['name'].count('и')}
+        return result
       end
     end
   end
